42d272bef7ba8aaefb3fa3c3d73e77823aa68b38
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-02-19 16:48:04 +0100

    general tests

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-// const creatApp = Vue.creatApp;
-// stessa cosa di scrivere:
+
+
 const { createApp } = Vue
 
 createApp({
@@ -34,9 +34,30 @@ createApp({
             ],
       }
     },methods:{
+        nextSlide() {
+        this.activeSlideIndex++;
 
-    }
-  }).mount('#app')
+        // controllo che l'indice non vada oltre l'ultima immagine
+        if (this.activeSlideIndex >= this.movie.images.length) {
+            this.activeSlideIndex = 0
+        }
+    },
+
+    prevSlide() {
+        this.activeSlideIndex--;
+
+        // qui controllo che l'indice non vada oltre lo zero 
+        if (this.activeSlideIndex < 0) {
+            this.activeSlideIndex = this.movie.images.length - 1;
+        }
+    },
+
+    changeSlide(index) {
+        console.log(index)
+        this.activeSlideIndex = index
+    },
+}
+}).mount('#app');
 
 
 
